2256e9d3fe35cd9aeb7d23cd4e05307f72960f62
Priyanshu  Padeliya <79414782+priyanshu30122001@users.noreply.github.com>
2024-04-19 00:50:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-  // Display and hide the rule 
-  var pcChoice = '';
+   var pcChoice = '';
   const nextButton = document.getElementById("next"); 
   const ruleButton = document.getElementById("pop-up");    
   const ruleBoard = document.getElementById("Rules");
@@ -7,13 +6,14 @@
   const playerScore = document.getElementById('yourScore');
   const computerScore = document.getElementById('computerScore');
   const resetGame =document.getElementById('reset');
+  const borderColor = document.getElementById("playerBanner");
+  const cardBorder =document.getElementById("computerBanner");
 
   var p = 0;
   var pc =0;
 
-  
+  // display and hides the rules 
   function displayRules(){
-      // let x = document.getElementById("Rules");
       if(ruleBoard.style.visibility === 'visible'){
          ruleBoard.style.visibility = 'hidden';
       }
@@ -21,57 +21,58 @@
           ruleBoard.style.visibility = 'visible';
       }    
      }
-  
+    
+  // mininizes the rule board    
   function minimize(){ruleBoard.style.visibility = 'hidden';      }
+  
   // randamize a number from 1 to 3
   function getRandomInt(max) {
       return Math.floor(Math.random() * Math.floor(max));
       }
-      
+
+  // function which executes after the clicking the rock paper scissors button      
   function functionToExecute(id) {
 
       const playerChoice = id;
-      console.log(pcChoice);
-      console.log(playerChoice);
       let winner = document.getElementById("result_text");
-      const resultText = document.getElementById("against");
+      const againstText = document.querySelector(".against");
       document.querySelector(".triangle-section").classList.toggle("hidden");
       document.querySelector(".results").classList.toggle("hidden");
       displayResults(playerChoice,pcChoice);
       if (playerChoice === pcChoice) {
-           console.log("Draw");
+           
            winner.innerText="TIE UP";
+           againstText.innerText="  ";
            button.innerText='REPLAY';
-        //    changeScore();  
-           return "It's a tie!";
+         
       }
       else if ((playerChoice === "rock" && pcChoice === "scissors") ||
                (playerChoice === "paper" && pcChoice === "rock") ||
                (playerChoice === "scissors" && pcChoice === "paper")) {
                 
           addNextbutton();
-          console.log("you win !!");
           winner.innerText="YOU WIN";
+          againstText.innerText="AGAINST PC";
           button.innerText= "PLAY AGAIN";
           p++;
           changeScore();
-          return "You win!";
+          borderColor.style.boxShadow="0 0 0 35px rgba(46, 154, 37, 0.5), 0 0 0 60px rgba(46, 154, 37, 0.4), 0 0 0 80px rgba(46, 154, 37, 0.2)";
     
       } 
       else {
           console.log("PC win !!!");
           winner.innerText="YOU LOST";
+          againstText.innerText="AGAINST PC";
           button.innerText= "PLAY AGAIN";
           pc++;
           changeScore();
-          return "Computer wins!";
+          cardBorder.style.boxShadow="0 0 0 35px rgba(46, 154, 37, 0.5), 0 0 0 60px rgba(46, 154, 37, 0.4), 0 0 0 80px rgba(46, 154, 37, 0.2)";
+          
 
       }   
 
   }
   function changeScore(){
-    // console.log(playerScore.innerText);
-    // console.log(computerScore.innerText);
     playerScore.innerText=p;
     computerScore.innerText=pc;
   }
@@ -84,7 +85,7 @@
   //function player card
   function playerbanner(playerChoice){
       const playerCard = document.getElementById('player');
-      const borderColor = document.getElementById("playerBanner");
+     
       if(playerChoice == 'rock'){
           playerCard.src="images/icons8-fist-67 1.svg";
           borderColor.style.borderColor ="rgba(0, 116, 182, 1)";
@@ -104,7 +105,6 @@
   
   function computerTurn(){
       var y = getRandomInt(3); 
-      const cardBorder =document.getElementById("computerBanner");
       const pcCard = document.getElementById('computer');
           switch (y) {
           case 0:
@@ -129,9 +129,6 @@
       playerbanner(playerChoice);
       computerTurn();
      
-      
-      // const change = document.getElementById('pop-up');
-
   }
   
   button.addEventListener('click', () => { 
@@ -140,6 +137,8 @@
       document.querySelector(".results").classList.toggle("hidden");
       nextButton.style.visibility=("hidden");
       ruleButton.style.right="3rem";
+      cardBorder.style.boxShadow="none";
+      borderColor.style.boxShadow="none";
 
     },500)
   });
@@ -165,7 +164,18 @@
             winner.innerText="YOU WON THE GAME";
           }
   });
-
+ 
+  resetGame.addEventListener('click',()=>{
+    document.querySelector(".winner_result").classList.toggle("hidden");
+    document.querySelector(".triangle-section").classList.toggle("hidden");
+    document.querySelector("header").classList.toggle("hidden");
+    p=0
+    pc=0
+    playerScore.innerText=p;
+    computerScore.innerText=pc;
+    cardBorder.style.boxShadow="none";
+    borderColor.style.boxShadow="none";
+ });
   
       
 
